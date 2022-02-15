74b611056247a78066b0d278dba874a7c9dd59fc
Gabriel Gerlero <gerlero@users.noreply.github.com>
2022-02-15 18:21:18 -0300

    openfoam2112: update to 1.0.2 (#3)

--- Casks/openfoam2112.rb
+++ Casks/openfoam2112.rb
@@ -1,12 +1,12 @@
 cask "openfoam2112" do
   arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
 
-  version "1.0"
+  version "1.0.2"
 
   if Hardware::CPU.intel?
-    sha256 "ad69c7366af410072b0ffac98b592f038285b8856d20b51a5e19c83f7272e274"
+    sha256 "07a63b3d05068d4c943cf9c63f5f9cf9c20ec8a8b713702ccf30b3745c05ea78"
   else
-    sha256 "4c0dc5b2025019aec4c0ebc2f02c7f4f9e5f874754a4fb55f6717a14fed66f69"
+    sha256 "1c7a30b514f3871c374dd8dc89bdbc5b59bf4b30bc51e74abe99e4b1d509c856"
   end
 
   url "https://github.com/gerlero/openfoam2112-app/releases/download/v#{version}/openfoam2112-app-#{arch}.zip"
