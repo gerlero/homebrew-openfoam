e424c19e88252ac02091c59f09c4f4aec8abd443
Gabriel Gerlero <gerlero@users.noreply.github.com>
2022-02-18 22:32:11 -0300

    openfoam2112: update to 1.2.0

--- Casks/openfoam2112.rb
+++ Casks/openfoam2112.rb
@@ -1,12 +1,12 @@
 cask "openfoam2112" do
   arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
 
-  version "1.1.0"
+  version "1.2.0"
 
   if Hardware::CPU.intel?
-    sha256 "cc1d75b7d9ff6863fe1b84196f9e6cbf0af5abdfacbd4b62cb6639dfaaee21ae"
+    sha256 "20b9594f4c5bdbb44b26e9b212aaf89370f2bdddfe1a25d7835d326a1f078d39"
   else
-    sha256 "19110c20f8e60f746cf4132dd02ca5fa5851fab1495658f7d5b5f92a26bab286"
+    sha256 "604b9262c04dc8d9769119bc06f3a8477e73adeda584157f546070345ffa34e6"
   end
 
   url "https://github.com/gerlero/openfoam2112-app/releases/download/v#{version}/openfoam2112-app-#{arch}.zip"
@@ -28,7 +28,7 @@ cask "openfoam2112" do
   depends_on formula: "fftw"
   depends_on formula: "kahip"
   depends_on formula: "metis"
-  depends_on formula: "scotch"
+  depends_on formula: "gerlero/openfoam/scotch-no-pthread"
 
   app "OpenFOAM-v2112.app"
   binary "#{appdir}/OpenFOAM-v2112.app/Contents/MacOS/openfoam", target: "openfoam2112"
