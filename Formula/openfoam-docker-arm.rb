d0906b84b82840f085f252e8ecbc6cc647477599
Gabriel Gerlero <gerlero@users.noreply.github.com>
2022-04-16 22:43:45 -0300

    openfoam-docker-arm 2022-01-10
    Closes #18.
    
    Signed-off-by: Gabriel Gerlero <15150530+gerlero@users.noreply.github.com>

--- Formula/openfoam-docker-arm.rb
+++ Formula/openfoam-docker-arm.rb
@@ -1,9 +1,9 @@
 class OpenfoamDockerArm < Formula
   desc "Run script for ARM-based OpenFOAM container images"
   homepage "https://github.com/gerlero/openfoam-docker-arm/"
-  url "https://raw.githubusercontent.com/gerlero/openfoam-docker-arm/c88703d81b3a8c39bc19d3844c2927f2517250a4/openfoam-docker-arm"
-  version "2021-12-21"
-  sha256 "db44872ed03ebacee2d1ef47ffc33ee327251f51fb54f42769cb0242b2a0b3d7"
+  url "https://raw.githubusercontent.com/gerlero/openfoam-docker-arm/3f91fefb84989ff2ae5376978458508c7887dbba/openfoam-docker-arm"
+  version "2022-01-10"
+  sha256 "748e6f98b4a84a06a6d98b4c1c55582f42387162e4315c09827aeedf86df21c9"
   license "GPL-3.0-or-later"
   head "https://raw.githubusercontent.com/gerlero/openfoam-docker-arm/main/openfoam-docker-arm"
 
@@ -13,7 +13,7 @@ class OpenfoamDockerArm < Formula
   end
 
   bottle do
-    root_url "https://github.com/gerlero/homebrew-openfoam/releases/download/openfoam-docker-arm-2021-12-21"
+    root_url "https://github.com/gerlero/homebrew-openfoam/releases/download/openfoam-docker-arm-2022-01-10"
     sha256 cellar: :any_skip_relocation, big_sur:      "380ab1adf2f633a211bfee1c39f1acb0a3e21ed54784f77f5def14366c8aca9f"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "a5c9649baff4d8ed82462394f0a25369b17d6b9148afc65c4e2a994a50ce84de"
   end
