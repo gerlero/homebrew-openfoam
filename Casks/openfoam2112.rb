f20d3ff8ce3dfee5dccb29841a403e6d0cdc208e
Gabriel Gerlero <gerlero@users.noreply.github.com>
2022-05-15 17:22:08 -0300

    Update openfoam2112 from 1.5.4 to 1.6.0 (#24)
    * Update openfoam2112 from 1.5.4 to 1.6.0
    
    * Update uninstall stanza

--- Casks/openfoam2112.rb
+++ Casks/openfoam2112.rb
@@ -1,12 +1,12 @@
 cask "openfoam2112" do
   arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
 
-  version "1.5.4"
+  version "1.6.0"
 
   if Hardware::CPU.intel?
-    sha256 "5e87350b34bd171fcb12d8d09430323172c11ec76568dba3b61bec652c3d192d"
+    sha256 "8a0c0230248f7fda0787715e203bee0617be8268de2b4a210f5ae559af57a1b0"
   else
-    sha256 "5bff043e8b282b659ab8d9bccfcc3e21dff491a146256f8e30901002a490846f"
+    sha256 "bbf886b37eebd1d1e18635d45b979f6cdb6787d1b0082666185c360605c1e001"
   end
 
   url "https://github.com/gerlero/openfoam2112-app/releases/download/v#{version}/openfoam2112-app-#{arch}.zip"
@@ -34,8 +34,8 @@ cask "openfoam2112" do
   binary "#{appdir}/OpenFOAM-v2112.app/Contents/MacOS/openfoam", target: "openfoam2112"
 
   uninstall script: {
-    executable:   "hdiutil",
-    args:         ["detach", "-quiet", "/Volumes/OpenFOAM-v2112"],
+    executable:   "#{staged_path}/OpenFOAM-v2112.app/Contents/MacOS/volume",
+    args:         ["eject"],
     must_succeed: false,
   }
 end
