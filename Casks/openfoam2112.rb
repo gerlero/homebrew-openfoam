d30a809a0997269dfaa31407b9c49f2519362281
Gabriel Gerlero <gerlero@users.noreply.github.com>
2023-05-29 08:43:19 -0300

    openfoam2112 1.10.0

--- Casks/openfoam2112.rb
+++ Casks/openfoam2112.rb
@@ -1,11 +1,11 @@
 cask "openfoam2112" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "1.9.0"
-  sha256 arm:   "fbbdff20450bdd18a7ee3078a361afd42be424ac97a4e5cfb49c32a2f1e33e8a",
-         intel: "88925bce32017609305b02a69291b8f86f56db5b110fb70763d3d90380f897dd"
+  version "1.10.0"
+  sha256 arm:   "32ebc7b23f9eeebd00a99d58be5fe6db23927d81f914b3f7d96302d06672a5d7",
+         intel: "06692a1d3a7877469e5e388a5a31fd3275af6da9d976cd47a667c795773d75e1"
 
-  url "https://github.com/gerlero/openfoam-app/releases/download/v#{version}/openfoam2112-app-homebrew-#{arch}.zip"
+  url "https://github.com/gerlero/openfoam-app/releases/download/v#{version}/openfoam2112-app-#{arch}.zip"
   name "OpenFOAM v2112"
   desc "Open-source toolbox for Computational Fluid Dynamics (CFD)"
   homepage "https://github.com/gerlero/openfoam-app/"
