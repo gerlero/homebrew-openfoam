734d0fdbaef27ae8def2be78bf40fe5402aa009f
Gabriel Gerlero <gerlero@users.noreply.github.com>
2023-05-29 19:25:34 -0300

    openfoam2212 1.10.0 (#62)
    * openfoam2212 1.10.0
    
    * openfoam2212: remove all dependencies

--- Casks/openfoam2212.rb
+++ Casks/openfoam2212.rb
@@ -1,11 +1,11 @@
 cask "openfoam2212" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "1.9.0"
-  sha256 arm:   "db75ffee0097617f26762c96f8f332ee0a1984c6683d18e1b85e76e64b59f079",
-         intel: "9bc210ce6cc2c360390c75c48187f0fb6e349f9ef879271423490dd1bd57402a"
+  version "1.10.0"
+  sha256 arm:   "d50fffbea5bcec8bccca2eca4a91926b2e01dbabf299a73f99f81b41c7f2e02e",
+         intel: "2fc579c6c15c90ae23653a3f310a043dae078d093b9ff3cb4a6b0957bd8b5fd8"
 
-  url "https://github.com/gerlero/openfoam-app/releases/download/v#{version}/openfoam2212-app-homebrew-#{arch}.zip"
+  url "https://github.com/gerlero/openfoam-app/releases/download/v#{version}/openfoam2212-app-#{arch}.zip"
   name "OpenFOAM v2212"
   desc "Open-source toolbox for Computational Fluid Dynamics (CFD)"
   homepage "https://github.com/gerlero/openfoam-app/"
@@ -15,16 +15,6 @@ cask "openfoam2212" do
     strategy :github_latest
   end
 
-  depends_on formula: "bash"
-  depends_on formula: "open-mpi"
-  depends_on formula: "libomp"
-  depends_on formula: "boost"
-  depends_on formula: "fftw"
-  depends_on formula: "kahip"
-  depends_on formula: "metis"
-  depends_on formula: "gerlero/openfoam/cgal@4"
-  depends_on formula: "gerlero/openfoam/scotch-no-pthread"
-
   app "OpenFOAM-v2212.app"
   binary "#{appdir}/OpenFOAM-v2212.app/Contents/Resources/etc/openfoam", target: "openfoam2212"
 
@@ -33,4 +23,11 @@ cask "openfoam2212" do
     args:         ["eject"],
     must_succeed: false,
   }
+
+  caveats <<~EOS
+    As of #{token} 1.10.0, the app is fully self-contained.
+    If you are upgrading from a previous release, consider running:
+      brew autoremove
+    to uninstall Homebrew dependencies that are no longer required.
+  EOS
 end
