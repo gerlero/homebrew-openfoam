ec13a4136a692edceda57322cb9922f709b9aaf9
Gabriel Gerlero <gerlero@users.noreply.github.com>
2023-05-31 16:37:14 -0300

    Extract kahip formula

---
+++ Formula/kahip.rb
@@ -0,0 +1,30 @@
+class Kahip < Formula
+  desc "Karlsruhe High Quality Partitioning"
+  homepage "https://algo2.iti.kit.edu/documents/kahip/index.html"
+  url "https://github.com/KaHIP/KaHIP/archive/v3.15.tar.gz"
+  sha256 "20760099370ddf7ecb2f92bfdb727def48f6428001165be6ce504264b9a99a0b"
+  license "MIT"
+  head "https://github.com/KaHIP/KaHIP.git", branch: "master"
+
+  depends_on "cmake" => :build
+  depends_on "open-mpi"
+
+  on_macos do
+    depends_on "gcc"
+  end
+
+  fails_with :clang do
+    cause "needs OpenMP support"
+  end
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    output = shell_output("#{bin}/interface_test")
+    assert_match "edge cut 2", output
+  end
+end
