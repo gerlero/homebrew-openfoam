54dac2d6796f8dc76650dca4094c0843371192cf
Gabriel Gerlero <gerlero@users.noreply.github.com>
2023-10-06 18:31:18 -0300

    cgal@4: deprecate (#89)

--- Formula/cgal@4.rb
+++ Formula/cgal@4.rb
@@ -20,6 +20,8 @@ class CgalAT4 < Formula
 
   keg_only :versioned_formula
 
+  deprecate! date: "2023-10-06", because: :versioned_formula
+
   depends_on "cmake" => [:build, :test]
   depends_on "boost"
   depends_on "eigen"
