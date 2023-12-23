12a95aa79c33d1842df49a5b88257f5cae6a1a1c
Gabriel Gerlero <gerlero@users.noreply.github.com>
2023-12-23 15:55:33 -0300

    openfoam2312: new cask

---
+++ Casks/openfoam2312.rb
@@ -0,0 +1,28 @@
+cask "openfoam2312" do
+  arch arm: "arm64", intel: "x86_64"
+
+  version "1.12.0"
+  sha256 arm:   "5fec271cb9776372f8d4dc8f4e725cbea950bcea9d43c09c8489c54ed14a7893",
+         intel: "2681c9e52ad1af6a5c4110f7499e07dd5c5276a1278849f6c2ee57e77a5c8829"
+
+  url "https://github.com/gerlero/openfoam-app/releases/download/v#{version}/openfoam2312-app-#{arch}.zip"
+  name "OpenFOAM v2312"
+  desc "Open-source toolbox for Computational Fluid Dynamics (CFD)"
+  homepage "https://github.com/gerlero/openfoam-app/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  conflicts_with cask: "openfoam"
+
+  app "OpenFOAM-v2312.app"
+  binary "#{appdir}/OpenFOAM-v2312.app/Contents/Resources/etc/openfoam", target: "openfoam2312"
+
+  uninstall script: {
+    executable:   "#{staged_path}/OpenFOAM-v2312.app/Contents/Resources/volume",
+    args:         ["eject"],
+    must_succeed: false,
+  }
+end
