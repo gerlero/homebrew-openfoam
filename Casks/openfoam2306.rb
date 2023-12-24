95e379c6275c017ce94ad1242914256ef80a75a7
Gabriel Gerlero <gerlero@users.noreply.github.com>
2023-12-24 20:15:46 -0300

    openfoam2306 1.12.1 (#99)

--- Casks/openfoam2306.rb
+++ Casks/openfoam2306.rb
@@ -1,9 +1,9 @@
 cask "openfoam2306" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "1.12.0"
-  sha256 arm:   "c603ec3cdb2681e62620277f5dec85e81e22cd0cfb7a8363e30d43c15743f004",
-         intel: "44d9ff48c07aca3f34185dda5ef4a3523422e0525cf649e85e3505f71db4b4bd"
+  version "1.12.1"
+  sha256 arm:   "00a7c31fa6a095335766a2af2a5e8015329e66669a5a04a26e8055dc2a6c2d25",
+         intel: "8dc279f7936a21fa52249602565b00c1622752703b2e3d45c02f23853c45b6e6"
 
   url "https://github.com/gerlero/openfoam-app/releases/download/v#{version}/openfoam2306-app-#{arch}.zip"
   name "OpenFOAM v2306"
