ca99107cf5230b408cdb2ff8ab0d32cfc7cb67f1
Gabriel Gerlero <gerlero@users.noreply.github.com>
2025-05-24 17:31:06 -0300

    foamlib 1.0.0 (new formula) (#157)

---
+++ Formula/foamlib.rb
@@ -0,0 +1,60 @@
+class Foamlib < Formula
+  include Language::Python::Virtualenv
+
+  desc "Modern Python package for interacting with OpenFOAM"
+  homepage "https://github.com/gerlero/foamlib"
+  url "https://files.pythonhosted.org/packages/8d/82/d7d520fe761b46352be08ae999cf5f2fd32d060ace7d44739fd07db24626/foamlib-1.0.0.tar.gz"
+  sha256 "830881df1d16f569eda236e7c066199915e04f1041bcb5fc279e2afe21f0c3e0"
+  license "GPL-3.0-only"
+  head "https://github.com/gerlero/foamlib.git", branch: "main"
+
+  depends_on "numpy"
+  depends_on "python@3.13"
+
+  resource "aioshutil" do
+    url "https://files.pythonhosted.org/packages/75/e4/ef86f1777a9bc0c51d50487b471644ae20941afe503591d3a4c86e456dac/aioshutil-1.5.tar.gz"
+    sha256 "2756d6cd3bb03405dc7348ac11a0b60eb949ebd63cdd15f56e922410231c1201"
+  end
+
+  resource "markdown-it-py" do
+    url "https://files.pythonhosted.org/packages/38/71/3b932df36c1a044d397a1f92d1cf91ee0a503d91e470cbd670aa66b07ed0/markdown-it-py-3.0.0.tar.gz"
+    sha256 "e3f60a94fa066dc52ec76661e37c851cb232d92f9886b15cb560aaada2df8feb"
+  end
+
+  resource "mdurl" do
+    url "https://files.pythonhosted.org/packages/d6/54/cfe61301667036ec958cb99bd3efefba235e65cdeb9c84d24a8293ba1d90/mdurl-0.1.2.tar.gz"
+    sha256 "bb413d29f5eea38f31dd4754dd7377d4465116fb207585f97bf925588687c1ba"
+  end
+
+  resource "pygments" do
+    url "https://files.pythonhosted.org/packages/7c/2d/c3338d48ea6cc0feb8446d8e6937e1408088a72a39937982cc6111d17f84/pygments-2.19.1.tar.gz"
+    sha256 "61c16d2a8576dc0649d9f39e089b5f02bcd27fba10d8fb4dcc28173f7a45151f"
+  end
+
+  resource "pyparsing" do
+    url "https://files.pythonhosted.org/packages/bb/22/f1129e69d94ffff626bdb5c835506b3a5b4f3d070f17ea295e12c2c6f60f/pyparsing-3.2.3.tar.gz"
+    sha256 "b9c13f1ab8b3b542f72e28f634bad4de758ab3ce4546e4301970ad6fa77c38be"
+  end
+
+  resource "rich" do
+    url "https://files.pythonhosted.org/packages/a1/53/830aa4c3066a8ab0ae9a9955976fb770fe9c6102117c8ec4ab3ea62d89e8/rich-14.0.0.tar.gz"
+    sha256 "82f1bc23a6a21ebca4ae0c45af9bdbc492ed20231dcb63f297d6d1021a9d5725"
+  end
+
+  def python3
+    which("python3.13")
+  end
+
+  def install
+    venv = virtualenv_create(libexec, python3)
+    venv.pip_install resources
+    system python3, "-m", "pip", "--python=#{venv.root}", "install",
+                                 *std_pip_args(prefix: false, build_isolation: true), "."
+
+    (prefix/Language::Python.site_packages(python3)/"homebrew-foamlib.pth").write venv.site_packages
+  end
+
+  test do
+    system python3.to_s, "-c", "import foamlib"
+  end
+end
