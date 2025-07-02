a4d2f2996cc5b3358a5dab8f4b2259c29d1cd013
Gabriel Gerlero <gerlero@users.noreply.github.com>
2025-07-02 11:11:03 -0300

    openfoam@2412 2.1.1 (#162)

--- Casks/openfoam@2412.rb
+++ Casks/openfoam@2412.rb
@@ -1,9 +1,9 @@
 cask "openfoam@2412" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "2.1.0"
-  sha256 arm:   "7e878797d5261f9bbcf8da0437b543448f992e137d36b1321a233746db291a97",
-         intel: "206731881ab08e074d49c4680978c14068c19da80d8705a6289e2b777578d851"
+  version "2.1.1"
+  sha256 arm:   "77e7f640eebb290eb13e658779d034d6bf8813c1b58c0577e54164dd7ef50acd",
+         intel: "682ab2087d56e09a85501abe36654a4a627b089f1bb6b0d244e83466eef179de"
 
   url "https://github.com/gerlero/openfoam-app/releases/download/v#{version}/openfoam2412-app-#{arch}.zip"
   name "OpenFOAM v2412"
@@ -14,8 +14,6 @@ cask "openfoam@2412" do
     url :url
     strategy :github_latest
   end
-
-  conflicts_with cask: "openfoam"
 
   app "OpenFOAM-v2412.app"
   binary "#{appdir}/OpenFOAM-v2412.app/Contents/Resources/etc/openfoam", target: "openfoam2412"
