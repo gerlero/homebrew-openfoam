6d55c2d1c84c34cf3d6bff14237157b30925f9fe
Gabriel Gerlero <gerlero@users.noreply.github.com>
2026-02-03 00:39:55 -0300

    Add back last Intel version of openfoam@2506

--- Casks/openfoam@2506.rb
+++ Casks/openfoam@2506.rb
@@ -1,8 +1,16 @@
 cask "openfoam@2506" do
-  version "2.1.3"
-  sha256 "fc1450f19a6615d85061856062fd76a9d880f9000a0768a4d819834489e13878"
+  arch arm: "arm64", intel: "x86-64"
 
-  url "https://github.com/gerlero/openfoam-app/releases/download/v#{version}/openfoam2506-app-arm64.zip"
+  on_arm do
+    version "2.1.3"
+    sha256 "fc1450f19a6615d85061856062fd76a9d880f9000a0768a4d819834489e13878"
+  end
+  on_intel do
+    version "2.1.2"
+    sha256 "7cbee661ad4cf09e4cf9e45cbdd69a9b21c1415c01c86c81207c28b70d31bcdc"
+  end
+
+  url "https://github.com/gerlero/openfoam-app/releases/download/v#{version}/openfoam2506-app-#{arch}.zip"
   name "OpenFOAM v2506"
   desc "Open-source toolbox for Computational Fluid Dynamics (CFD)"
   homepage "https://github.com/gerlero/openfoam-app/"
@@ -11,8 +19,6 @@ cask "openfoam@2506" do
     url :url
     strategy :github_latest
   end
-
-  depends_on arch: :arm64
 
   app "OpenFOAM-v2506.app"
   binary "#{appdir}/OpenFOAM-v2506.app/Contents/Resources/etc/openfoam", target: "openfoam2506"
