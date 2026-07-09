2ca2ac69fac118b94e861b8eb95cd476ae305df5
Gabriel Gerlero <gerlero@users.noreply.github.com>
2026-07-08 22:01:51 -0300

    Use fully qualified names in conflicts_with (#208)

--- Casks/openfoam@2606.rb
+++ Casks/openfoam@2606.rb
@@ -12,7 +12,7 @@ cask "openfoam@2606" do
     strategy :github_latest
   end
 
-  conflicts_with cask: "openfoam"
+  conflicts_with cask: "gerlero/openfoam/openfoam"
   depends_on arch: :arm64
   depends_on :macos
 
